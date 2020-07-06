284f3b9ff7f1b43f7e462a5cd5d675619a71c461
mncool <wsbhgd@163.com>
2020-07-07 00:44:21 +0800

    leetcode 1051

---
+++ src/main/java/leetcode/array/HeightChecker.java
@@ -0,0 +1,36 @@
+package leetcode.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author mncool
+ */
+public class HeightChecker {
+
+    public static void main(String[] args) {
+        int[] heights = {1, 1, 4, 2, 1, 3};
+        System.out.println(heightChecker(heights));
+    }
+
+    public static int heightChecker(int[] heights) {
+        if (heights.length <= 1) {
+            return 0;
+        }
+        int[] sortedElements = new int[101];
+        for (int i = 0; i < heights.length; i++) {
+            sortedElements[heights[i]]++;
+        }
+        int count = 0;
+        int start = 0;
+        for (int i = 0; i < sortedElements.length; i++) {
+            while (sortedElements[i] -- > 0){
+                if (heights[start] != i) {
+                    count++;
+                }
+                start++;
+            }
+        }
+        return count;
+    }
+}
